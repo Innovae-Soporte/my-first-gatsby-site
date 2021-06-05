e084a46f4a174d083d098049f3a1526275bc5060
Innovae-Soporte <soporte@innovae.com>
2021-06-05 12:27:01 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({data}) => {
   return (
     <Layout pageTitle="Home Page">
-    <p>Estoy haciendo mi primer sitio en gatsby,  segunda parte</p>
+    <p>{ data.allNodeArticle.edges[0].node.title } Estoy haciendo mi primer sitio en gatsby,  segunda parte</p>
     <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
@@ -14,5 +15,23 @@ const IndexPage = () => {
     </Layout>
   )
 }
+export const query = graphql`
+query ArticlePage {
+  allNodeArticle {
+    edges {
+      node {
+        title
+        id
+        body {
+          value
+          format
+          processed
+          summary
+        }
+      }
+    }
+  }
+}
 
+`
 export default IndexPage
